e54caca64b2a118bad52656c3fb61c02a0485c18
Lê Võ Quang Vinh <L V Quang Vinh>
2016-03-17 15:00:44 +0700

    Tai thu lan 2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            Nhap(out b);         
+       
+
+        }
+        public static void Nhap( out int a)
+        {
+            try
+            {
+                Console.Write("Nhap so nguyen a: "); a = int.Parse(Console.ReadLine());
+            }
+            catch(Exception sx)
+            {
+                Console.WriteLine("Kieu du lieu nhap khong hop le !");
+                Nhap(out a);
+            }
+        }
+    }
+}
